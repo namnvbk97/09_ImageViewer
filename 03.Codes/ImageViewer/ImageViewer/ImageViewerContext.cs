6490fca7770b2e06dacca06e67c1df1d4b89db3b
NAM <nam.nv.bk97@gmail.com>
2024-07-30 11:58:56 +0700

    first commit

---
+++ 03.Codes/ImageViewer/ImageViewer/ImageViewerContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+using WPF.Common;
+
+namespace Vision.Controls
+{
+    public class ImageViewerContext: NotifyPropertyChanged
+    {
+        private BitmapSource _image = null;
+
+        public BitmapSource Image
+        {
+            get => _image;
+            set
+            {
+                _image = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ImageViewerContext()
+        {
+
+        }
+    }
+}
---
+++ 03.Codes/ImageViewer/ImageViewerTest/MainContext.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+using Vision.Controls;
+using Vision.Image;
+using WPF.Common;
+
+namespace ImageViewerTest
+{
+    public class MainContext: NotifyPropertyChanged
+    {
+        #region User DataContext
+        private ImageViewerContext _imageContext = new ImageViewerContext();
+
+        public ImageViewerContext ImageContext
+        {
+            get => _imageContext;
+            set => _imageContext = value;
+        }
+        #endregion
+
+        #region Command
+        public RelayCommand LoadImageCommand { get; set; }
+        #endregion
+
+        #region Init
+        public MainContext() 
+        {
+            LoadImageCommand = new RelayCommand(OnLoadImageCommand);
+        }
+        #endregion
+
+        #region OnCommand
+        private void OnLoadImageCommand(object o)
+        {
+            Ookii.Dialogs.Wpf.VistaOpenFileDialog op = new Ookii.Dialogs.Wpf.VistaOpenFileDialog();
+            op.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp";
+            if (op.ShowDialog() == true)                            // Hiển thị form chọn file ảnh
+            { 
+                string file           = op.FileName;                // Lấy đường dẫn ảnh
+                BitmapSource bmsColor = BitmapSourceEx.Load(file);  // Load ảnh theo đường dẫn
+                ImageContext.Image    = bmsColor;                   // Hiển thị ảnh lên ImageViewer
+            }    
+           
+        }
+        #endregion
+
+    }
+}
